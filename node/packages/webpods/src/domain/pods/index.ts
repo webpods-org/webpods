b30397327fb42e6e29e8f18316b40a1efa3c4299
Jeswin Kumar <jeswinpk@agilehead.com>
2025-08-26 00:19:35 +0530

    refactor: Split domain functions into separate files
    - Added DataContext type with db field
    - Added Result utility functions (success/failure)
    - Added case conversion utilities (toSnakeCase/toCamelCase)
    - Split ratelimit domain into separate function files
    - Started splitting pods domain functions
    - Standardized parameter building with snake_case
    - Using consistent Error type instead of DomainError
    
    Following single-function-per-file pattern for better organization.

---
+++ node/packages/webpods/src/domain/pods/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Pod domain functions
+ */
+
+export { createPod } from "./create-pod.js";
+export { getPod } from "./get-pod.js";
+// TODO: Add other pod functions:
+// export { getPodOwner } from "./get-pod-owner.js";
+// export { transferPodOwnership } from "./transfer-pod-ownership.js";
+// export { deletePod } from "./delete-pod.js";
+// export { listPodStreams } from "./list-pod-streams.js";
---
+++ node/packages/webpods/src/domain/ratelimit/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Rate limiting domain functions
+ */
+
+export { incrementRateLimit } from "./increment-rate-limit.js";
+export type { RateLimitType } from "./increment-rate-limit.js";
+export { checkRateLimit } from "./check-rate-limit.js";
+export { getRateLimitStatus } from "./get-rate-limit-status.js";
